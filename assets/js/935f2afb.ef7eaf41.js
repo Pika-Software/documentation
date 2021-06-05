bd0781c567301e2f4ebdd30f0dbeaed6a9375326
Retro <44505837+dankmolot@users.noreply.github.com>
2021-06-05 18:23:15 +0000

    Deploy website - based on 969c32d67f072d5c1e19e1bde065450d994c388c

--- assets/js/935f2afb.ef7eaf41.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkpika_soft=self.webpackChunkpika_soft||[]).push([[53],{9054:function(e){"use strict";e.exports=JSON.parse('{"pluginId":"default","version":"current","label":"Next","isLast":true,"docsSidebars":{"welcome":[{"type":"link","label":"Welcome","href":"/"}]},"permalinkToSidebar":{"/":"welcome"}}')}}]);
