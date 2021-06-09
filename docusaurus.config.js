38da634e44b2b3270f6ba35e7424595fbc6629b1
Retro <44505837+dankmolot@users.noreply.github.com>
2021-06-09 14:24:28 +0300

    Changed favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
   organizationName: 'Pika-Software', // Usually your GitHub org/user name.
   projectName: 'pika-docs', // Usually your repo name.
   themeConfig: {
